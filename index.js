3b2206bc200db1880c5d80707ae740c1f77338f5
yuchen zhou <jasperz4520@gmail.com>
2024-09-16 09:49:37 -0700

    message

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#3-client-entrypoints
-import { connect, play } from './networking';
-import { startRendering, stopRendering } from './render';
-import { startCapturingInput, stopCapturingInput } from './input';
-import { downloadAssets } from './assets';
-import { initState } from './state';
-import { setLeaderboardHidden } from './leaderboard';
+import { connect, play } from './networking.js';
+import { startRendering, stopRendering } from './render.js';
+import { startCapturingInput, stopCapturingInput } from './input.js';
+import { downloadAssets } from './assets.js';
+import { initState } from './state.js';
+import { setLeaderboardHidden } from './leaderboard.js';
 
 // I'm using a tiny subset of Bootstrap here for convenience - there's some wasted CSS,
 // but not much. In general, you should be careful using Bootstrap because it makes it
