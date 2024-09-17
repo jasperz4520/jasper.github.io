086347244d75480d884d09f790d1a65b62f7bd35
yuchen zhou <jasperz4520@gmail.com>
2024-09-17 10:33:47 -0700

    message

--- index.js
+++ index.js
@@ -1,44 +1,18 @@
-// Learn more about this file at:
-// https://victorzhou.com/blog/build-an-io-game-part-1/#3-client-entrypoints
-import { connect, play } from './networking.js';
-import { startRendering, stopRendering } from './render.js';
-import { startCapturingInput, stopCapturingInput } from './input.js';
-import { downloadAssets } from './assets.js';
-import { initState } from './state.js';
-import { setLeaderboardHidden } from './leaderboard.js';
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// I'm using a tiny subset of Bootstrap here for convenience - there's some wasted CSS,
-// but not much. In general, you should be careful using Bootstrap because it makes it
-// easy to unnecessarily bloat your site.
-// import './css/bootstrap-reboot.css';
-// import './css/main.css';
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-const playMenu = document.getElementById('play-menu');
-const playButton = document.getElementById('play-button');
-const usernameInput = document.getElementById('username-input');
-console.log(111)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
-Promise.all([
-  connect(onGameOver),
-  downloadAssets(),
-]).then(() => {
-  playMenu.classList.remove('hidden');
-  usernameInput.focus();
-  playButton.onclick = () => {
-    console.log(111)
-    // Play!
-    play(usernameInput.value);
-    playMenu.classList.add('hidden');
-    initState();
-    startCapturingInput();
-    startRendering();
-    setLeaderboardHidden(false);
-  };
-}).catch(console.error);
-
-function onGameOver() {
-  stopCapturingInput();
-  stopRendering();
-  playMenu.classList.remove('hidden');
-  setLeaderboardHidden(true);
-}
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
