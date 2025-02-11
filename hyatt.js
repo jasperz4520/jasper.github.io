bf08295aa795b695757a70cf2126678e71bb26c0
yuchen zhou <jasperz4520@gmail.com>
2025-02-11 00:00:46 -0800

    message

---
+++ hyatt.js
@@ -0,0 +1,257 @@
+{
+    "profile": {
+        "full": {
+            "directBill": null,
+            "governmentRate": null,
+            "appMobileUsage": null,
+            "veteranStatus": null,
+            "veteranBranch": null,
+            "veteranRank": null,
+            "birthDate": null,
+            "gender": null,
+            "preferredCurrencyCode": null,
+            "registeredPromoCodes": [
+                "2K20N35K24",
+                "HPHHPROM24",
+                "EMEASP2X24",
+                "TLPFP0601C",
+                "TLPFP0601R",
+                "TLPTN0101R"
+            ],
+            "eligiblePromotions": [],
+            "accountNumber": "573347974O",
+            "userName": "",
+            "prefix": "",
+            "firstName": "YUCHEN",
+            "unicodeFirstName": null,
+            "middleName": "",
+            "lastName": "ZHOU",
+            "unicodeLastName": null,
+            "suffix": "",
+            "emailAddress": "JASPERZ4520@GMAIL.COM",
+            "language": "EN",
+            "points": 46409,
+            "lifePoints": 181409,
+            "ytdStays": 9,
+            "ytdNights": 22,
+            "ytdBasePoints": 30,
+            "profile": {
+                "tier": "V",
+                "tierQualificationDate": "20240611",
+                "tierExpireDate": "20260228",
+                "enrollDate": "20240301"
+            },
+            "chaseStatus": "OPEN",
+            "partner": {},
+            "AAANumber": null,
+            "isTemporaryPassword": "N",
+            "CRCodes": {
+                "preferredCR": null
+            }
+        },
+        "extended": {
+            "region": "USCAC",
+            "preferences": {
+                "row": {
+                    "destinations": {
+                        "destination": []
+                    },
+                    "me05_business_travel": "",
+                    "hotel_emails": {
+                        "hotel_email": [
+                            {
+                                "loc_code": "sjcjw"
+                            }
+                        ]
+                    },
+                    "preferred_hyatt_hotels": {
+                        "preferred_hyatt_hotel": [
+                            {
+                                "loc_code": "sjcjw"
+                            }
+                        ]
+                    },
+                    "me05_smoking": "NP",
+                    "me05_floor": "NP",
+                    "me05_female_traveler": "",
+                    "me05_bed_size": "NP",
+                    "me05_leisure_traveler": "Y",
+                    "me05_crib": "",
+                    "me05_meetings": "",
+                    "me05_mileage_offers": "",
+                    "me05_mileage_only": "",
+                    "me05_packages": "",
+                    "me05_preferred_hotels": "",
+                    "me05_persona": "",
+                    "me05_quiet_room": "",
+                    "me05_rollaway_bed": "",
+                    "me05_recv_hyatt_place_offers": "",
+                    "me05_hyatt_web_checkin": "",
+                    "me05_newsletter": "",
+                    "me05_recv_summerfield_suites_offer": "",
+                    "me05_recv_travel_newsletter": "",
+                    "me05_relaxation": "",
+                    "me05_remember_me": "",
+                    "me05_resorts": "",
+                    "me05_romance": "",
+                    "me05_senior_rates": "",
+                    "me05_shopping": "",
+                    "me05_ski": "",
+                    "me05_spa": "",
+                    "me05_top_travel_destinations": "",
+                    "me05_vacation_ownership": "",
+                    "me05_value_rates": "",
+                    "me05_weekends": "",
+                    "mobile_opt_in": "",
+                    "me05_recv_promos": "",
+                    "me05_name_distribution": "",
+                    "me05_mtg_dividends_participant": "",
+                    "me05_room_near_elevator": "",
+                    "me05_mobility_impaired": "",
+                    "me05_sight_impaired": "",
+                    "me05_hearing_impaired": "",
+                    "me05_recv_promo_email": "",
+                    "me05_recv_hyatt_extras_email": "",
+                    "me05_recv_text_only_email": "N",
+                    "me05_recv_esummaries": "Y",
+                    "me05_aaa_rates": "",
+                    "me05_american_express": "",
+                    "me05_bed_breakfast": "",
+                    "me05_customer_type": 0,
+                    "me05_family": "",
+                    "me05_glbt": "",
+                    "me05_golf": "",
+                    "me05_gp_offers": "",
+                    "me05_recv_hyatt_web_checkin_email": "Y",
+                    "me05_local_culture": ""
+                }
+            },
+            "profile": {
+                "tier_qualification_code": "V",
+                "tier_qualification_date": "20240611",
+                "max_activity_loc_codes": {
+                    "loc_code": []
+                }
+            },
+            "temporary_password_ind": "N",
+            "me02_enroll_date": "20240301",
+            "me02_tier": "V",
+            "me03_email_address_preference_ind": "Y",
+            "me02_exp_date": "20260228",
+            "me01_lang": "EN",
+            "me01_name_prefix": "",
+            "me01_first_name": "YUCHEN",
+            "me01_initial": "",
+            "me01_last_name_m": "ZHOU",
+            "me01_name_suffix": "",
+            "me01_company_name": "",
+            "tr06_stays_ytd": 9,
+            "tr06_nights_ytd": 22,
+            "tr06_curr_pts": 46409,
+            "tr06_life_pts": 181409,
+            "tr06_base_pts_ytd": 30,
+            "me01_company_title": "",
+            "me01_home_addr_line1": "2201 ARENA BLVD APT 7303",
+            "me01_home_addr_line2": "",
+            "me01_home_addr_line3": "",
+            "me01_home_city": "SACRAMENTO",
+            "me01_home_state": "CA",
+            "me01_home_country": "US",
+            "me01_home_postal_code": "95834",
+            "me01_bus_addr_line1": "",
+            "me01_bus_addr_line2": "",
+            "me01_bus_addr_line3": "",
+            "me01_bus_city": "",
+            "me01_bus_state": "",
+            "me01_bus_country": "",
+            "me01_bus_postal_code": "",
+            "email_address": "",
+            "me03_email_address": "JASPERZ4520@GMAIL.COM",
+            "uni_lang": "",
+            "me01_travel_agent_id": "",
+            "me01_travel_agency_name": "",
+            "me01_private_line_number": "",
+            "mobile_country_code": "",
+            "mobile_phone_number": "",
+            "me02_username": "",
+            "me02_enroll_src": "",
+            "dps_userid": "",
+            "me02_affil_code": "CHAS",
+            "me01_home_province": "",
+            "me01_bus_province": "",
+            "me01_comm_pref_addr": "H",
+            "me01_comm_pref_phone": "H",
+            "me01_home_phone_num": "13413339073",
+            "me01_home_phone_ext": "",
+            "me01_home_phone_country_code": "",
+            "me01_bus_phone_num": "",
+            "me01_bus_phone_ext": "",
+            "me01_bus_phone_country_code": "",
+            "chase_status": "OPEN",
+            "pr01_promo_status": {
+                "global_promo": [
+                    {
+                        "mc20_promo": "2K20N35K24",
+                        "pr00_registered": "Y",
+                        "pr01_awards_available": 0,
+                        "tr01_awards_earned": 0,
+                        "tr01_awards_redeemed": 0
+                    }
+                ],
+                "message_promo": [
+                    {
+                        "mc61_offer": "",
+                        "mc20_end_date": "20241215",
+                        "promo_registered": "Y",
+                        "promo_complete": "Y",
+                        "pr01_fulfill_cnt": "1",
+                        "pr01_stays_accum": "",
+                        "pr01_nights_accum": "",
+                        "mc20_promo": "HPHHPROM24"
+                    },
+                    {
+                        "mc61_offer": "",
+                        "mc20_end_date": "20241220",
+                        "promo_registered": "Y",
+                        "promo_complete": "N",
+                        "pr01_fulfill_cnt": "0",
+                        "pr01_stays_accum": "0.00",
+                        "pr01_nights_accum": "",
+                        "mc20_promo": "EMEASP2X24"
+                    },
+                    {
+                        "mc61_offer": "",
+                        "mc20_end_date": "20260331",
+                        "promo_registered": "Y",
+                        "promo_complete": "N",
+                        "pr01_fulfill_cnt": "0",
+                        "pr01_stays_accum": "",
+                        "pr01_nights_accum": "0.00",
+                        "mc20_promo": "TLPFP0601C"
+                    },
+                    {
+                        "mc61_offer": "",
+                        "mc20_end_date": "20260331",
+                        "promo_registered": "Y",
+                        "promo_complete": "Y",
+                        "pr01_fulfill_cnt": "1",
+                        "pr01_stays_accum": "",
+                        "pr01_nights_accum": "",
+                        "mc20_promo": "TLPFP0601R"
+                    },
+                    {
+                        "mc61_offer": "",
+                        "mc20_end_date": "20260331",
+                        "promo_registered": "Y",
+                        "promo_complete": "Y",
+                        "pr01_fulfill_cnt": "1",
+                        "pr01_stays_accum": "",
+                        "pr01_nights_accum": "",
+                        "mc20_promo": "TLPTN0101R"
+                    }
+                ]
+            },
+            "me02_mbr_acct": "573347974O"
+        }
+    }
+}
